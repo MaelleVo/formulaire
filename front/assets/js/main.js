ccf7a1dab9f60dabef999cdeb33e4eba36616975
foxy <maellevolz@gmail.com>
2024-05-31 15:04:32 +0200

    "add"

--- front/assets/js/main.js
+++ front/assets/js/main.js
@@ -7,13 +7,16 @@ $.addEventListener("DOMContentLoaded", () => {
 $.querySelector("#formulaire").addEventListener("submit", async (event) => {
   event.preventDefault();
 
-  const response = await axios.post("", {
-    firstname: $.querySelector("#firstname").value,
-    lastname: $.querySelector("#lastname").value,
-    email: $.querySelector("#email").value,
-    message: $.querySelector("#message").value,
-    subject: $.querySelector("#subject").value,
-  });
+  const response = await axios.post(
+    "https://sunny-kitsune-520ab6.netlify.app/send-form",
+    {
+      firstname: $.querySelector("#firstname").value,
+      lastname: $.querySelector("#lastname").value,
+      email: $.querySelector("#email").value,
+      message: $.querySelector("#message").value,
+      subject: $.querySelector("#subject").value,
+    }
+  );
   console.log(response.data);
   alert("Votre message à été envoyé");
 });
